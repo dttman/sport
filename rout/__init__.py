29846d1c9629d80b4966c4379c78d0356a2acb52
dtt3st <dtt3st@yandex.ru>
2022-01-20 09:39:13 +0300

    Change for Heroku

--- rout/__init__.py
+++ rout/__init__.py
@@ -7,7 +7,8 @@ from flask_fontawesome import FontAwesome
 #определение папок с хранением шаблонов и стилей
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 #Строка подключения к базе данных
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sport.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sport.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zqcqxjcwbdpnfi:e1e08e86c3d4e8f4e8a38d28032701fad5788b4e017fdf9af348d48aeb69e532@ec2-52-6-178-202.compute-1.amazonaws.com:5432/d39e2dl6jihnp1'
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
